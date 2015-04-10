0cce9a108d97b61bc36c1d6873a801ae5a02ee10
jamesgk <jamesgk19@gmail.com>
2015-04-10 14:57:03 -0700

    Add setup.py script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="booleanOperations",
+    version="0.1",
+    description="Boolean operations on paths.",
+    author="Frederik Berlaen",
+    url="https://github.com/typemytype/booleanOperations",
+    license="MIT",
+    packages=["booleanOperations"],
+    package_dir={"": "Lib"},
+    package_data={"booleanOperations": ["pyClipper.so"]}
+)
