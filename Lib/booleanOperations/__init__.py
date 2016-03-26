712111ddeecb8aec3a8ac30c6fc40ddfb3e98a4f
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-03-26 19:16:10 +0100

    [booleanOperations.__init__] export BooleanOperationManager static methods as module-level functions for easier import
    Fixes #24

--- Lib/booleanOperations/__init__.py
+++ Lib/booleanOperations/__init__.py
@@ -2,3 +2,10 @@ from __future__ import print_function, division, absolute_import
 from .booleanOperationManager import BooleanOperationManager
 
 __version__ = "0.2"
+
+# export BooleanOperationManager static methods
+union = BooleanOperationManager.union
+difference = BooleanOperationManager.difference
+intersection = BooleanOperationManager.intersection
+xor = BooleanOperationManager.xor
+getIntersections = BooleanOperationManager.getIntersections
