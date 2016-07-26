09a6f101bd43e5f0242b7fcda415fe914ac0c8a2
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-07-26 11:15:53 +0100

    version 0.4

--- Lib/booleanOperations/__init__.py
+++ Lib/booleanOperations/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, division, absolute_import
 from .booleanOperationManager import BooleanOperationManager
 
-__version__ = "0.3"
+__version__ = "0.4"
 
 # export BooleanOperationManager static methods
 union = BooleanOperationManager.union
