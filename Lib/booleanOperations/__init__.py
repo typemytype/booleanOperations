dbcf1e79aa8f0b52cb0da773b65d6984ab35643e
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-09-11 02:54:03 +0100

    .travis.yml: try once more to encrypt password, by passing the --repo slug
    I used the following command:
    
        echo <PASSWORD> | travis encrypt -R typemytype/booleanOperations --add deploy.password

--- Lib/booleanOperations/__init__.py
+++ Lib/booleanOperations/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, division, absolute_import
 from .booleanOperationManager import BooleanOperationManager
 
-__version__ = "0.5.0.dev3"
+__version__ = "0.5.0"
 
 # export BooleanOperationManager static methods
 union = BooleanOperationManager.union
