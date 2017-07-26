f3dca9779cfcf461a3da733f9b2a1dc08d5d4d2a
Cosimo Lupo <cosimo@anthrotype.com>
2017-07-26 19:51:01 +0100

    export '__version__' in top-level module
    This is so that all of fontmake's dependencies export a top-level
    '__version__' string, following PEP 396 recommendation.
    
    https://github.com/googlei18n/fontmake/issues/251
    https://github.com/googlei18n/fontmake/issues/310

--- Lib/booleanOperations/__init__.py
+++ Lib/booleanOperations/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division, absolute_import
 from .booleanOperationManager import BooleanOperationManager
 from .exceptions import BooleanOperationsError
+from .version import __version__
 
 # export BooleanOperationManager static methods
 union = BooleanOperationManager.union
